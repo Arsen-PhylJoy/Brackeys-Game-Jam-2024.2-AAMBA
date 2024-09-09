0259506b87b59b584826ffa0d16d199ecd9895c4
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-09-09 15:32:15 +0300

    Виртуальное передвижение hardcode MVP
    Включены плагины
    Импорт картинок для отладки
    Сцена для тестирования виртуального передвижения
    Клетка в виде ресурса
    Экран, который динамически меняет текстурку
    Виртуальная карта: обрабатывает движения игрока и содержит игрока
    Виртуальный игрок - просто контейнер для вектора передвижения и ХП. В будущем будет кислород и форсаж
    Добавлено 3 сигнала в Event bus для интерактивных элементов
    
    Комментарий: уровень захардкожен. То, как составлен урвоень можно посмотреть в файде virtual_map.gd или в Миро в доске с геймдизайном.
    Для СмоукТеста запускается сцена vmap_test_scene.tscn

---
+++ scenes/screen.gd
@@ -0,0 +1,14 @@
+extends Node3D
+
+@onready var _screen_mesh: MeshInstance3D = %ScreenMesh
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	_connect_signals()
+
+
+func _connect_signals() -> void:
+	(get_tree().current_scene as VirtualMap).forward_visual_changed.connect(_on_forward_visual_changed)
+
+func _on_forward_visual_changed(in_cell: Cell) -> void:
+	(_screen_mesh.get_surface_override_material(0) as ShaderMaterial).set_shader_parameter("visual", in_cell.visual)
