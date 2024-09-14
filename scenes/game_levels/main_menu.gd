4eadf7cd0924c66822425673f2f813bc62eb304f
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-09-14 15:39:58 +0300

    Sound settings

--- scenes/game_levels/main_menu.gd
+++ scenes/game_levels/main_menu.gd
@@ -1,7 +1,6 @@
 extends Node3D
 
 @onready var play: Button = %Play
-@onready var sound_settings: Button = %SoundSettings
 @onready var main_menu_music: AudioStreamPlayer = %MainMenuMusic
 
 # Called when the node enters the scene tree for the first time.
@@ -10,11 +9,7 @@ func _ready() -> void:
 
 func _connect_signals() -> void:
 	play.pressed.connect(_on_play_pressed)
-	sound_settings.pressed.connect(_on_sound_settings_pressed)
 
 func _on_play_pressed() -> void:
 	Global.index_current_level+=1
 	LevelManager.load_level(Global.levels[Global.index_current_level])
-
-func _on_sound_settings_pressed() -> void:
-	pass
