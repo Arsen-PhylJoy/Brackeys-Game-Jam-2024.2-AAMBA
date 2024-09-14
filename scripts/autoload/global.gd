3ccdc20344b8ae6ec3cca9b065793801a7f3ad35
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-09-14 23:04:51 +0300

    levels_to_test_events

--- scripts/autoload/global.gd
+++ scripts/autoload/global.gd
@@ -3,10 +3,8 @@ extends Node
 
 var index_current_level: int = 0
 @export var levels: Array[String] = [
-"res://scenes/game_levels/main_menu.tscn", 
-"res://scenes/game_levels/LEVEL_2.tscn",
-"res://scenes/game_levels/LEVEL_3.tscn",
-"res://scenes/game_levels/LEVEL_4.tscn",
-"res://scenes/game_levels/LEVEL_4_5_air.tscn",
-"res://scenes/game_levels/LEVEL_5.tscn"
+"res://scenes/game_levels/main_menu.tscn",
+"res://game.tscn",
+"res://game_2.tscn",
+"res://game_3.tscn"
 ]
