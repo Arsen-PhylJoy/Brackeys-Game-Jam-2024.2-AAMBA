5695ff77683f1972d252100579ffcdbbd4e456aa
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-09-15 07:00:23 +0300

    Фикс гео экрана

--- scenes/ui/geo.gd
+++ scenes/ui/geo.gd
@@ -1,7 +1,9 @@
 extends SubViewport
 
-@onready var regular: TextureRect = %Regular
-@onready var grid_container: GridCells = %RealGridContainer
+@export var is_real: bool = false
+
+@onready var regular: TextureRect = %UnrealRegular
+@onready var grid_container: GridCells = %UnrealGridContainer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,6 +13,5 @@ func _connect_signals() -> void:
 	(get_tree().get_nodes_in_group("the_most_important_node")[0] as VirtualEnvironment).vmap.visual_changed.connect(_on_visual_changed)
 
 func _on_visual_changed(in_cell: Cell, template: Array[Cell.cell_type]) -> void:
-	self.size = Vector2(4713,3535)
-	regular.texture = in_cell.visual
-	grid_container._update_grid(template)
+		self.size = Vector2(1440,1152)
+		regular.texture = in_cell.geo_visual
