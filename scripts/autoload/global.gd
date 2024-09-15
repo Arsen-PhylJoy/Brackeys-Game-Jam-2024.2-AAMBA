a014d1548bb8aae4a98495ae6a9f11ca52cf3dca
Arsen-PhylJoy <50301289+Arsen-PhylJoy@users.noreply.github.com>
2024-09-15 12:08:50 +0300

    уровни

--- scripts/autoload/global.gd
+++ scripts/autoload/global.gd
@@ -4,7 +4,10 @@ extends Node
 var index_current_level: int = 0
 @export var levels: Array[String] = [
 "res://scenes/game_levels/main_menu.tscn",
-"res://game.tscn",
-"res://game_2.tscn",
-"res://game_3.tscn"
+"res://Level_1.tscn",
+"res://Level_2.tscn",
+"res://Level_3.tscn",
+"res://Level_4.tscn",
+"res://Level_5.tscn",
+"res://Level_6.tscn"
 ]
